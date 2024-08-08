ab0f0ee12dd76db530fd6c64025c162f979f190a
mariaHelenaSalas <mariahelenasalas83@gmail.com>
2024-08-08 17:02:38 -0500

    mi primer commit

---
+++ funciones/ventas.py
@@ -0,0 +1,16 @@
+
+import funciones.globales as gf
+
+def ventas ():
+
+    title = """
+***********************************
+*          VENTAS                 *
+***********************************
+"""
+gf.borrar_pantalla()
+print(title)
+fecha_de_la_venta = input("ingrese la fecha de venta")
+informacion_del_cliente = ("ingrese el nombre y direccion")
+empleado_realizo_venta = ("ingrese el nombre y cargo")
+productos_vendido = ("ingrese nombre cantidad precio")
